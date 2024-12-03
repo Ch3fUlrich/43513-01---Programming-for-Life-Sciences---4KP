bafbaf15763f633233d4997c3c98cbca05262b58
Sergej Maul <maulser@gmail.com>
2024-12-03 11:50:49 +0100

    updated tests to new folder stracture

--- Project/tests/test_gillespie_simulation.py
+++ Project/tests/test_gillespie_simulation.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from Project.GillespieSimulation import StateMachine, State, Molecule, Complex
+from Project.classes.State import StateMachine, State
 
 
 @pytest.fixture
